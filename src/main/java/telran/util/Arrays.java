38ed28a4c83382d7b76fb13200dcbddfb40962cb
Tomer Peceniski <tomerpeceniski@gmail.com>
2024-07-14 18:41:28 +0300

    Adding the homework description and updating to yuri's resolution

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -1,50 +1,99 @@
 package telran.util;
 
 public class Arrays {
-    public static int search(int[] ar, int key) {
-        int index = 0;
-        while(index < ar.length && key != ar[index]) {
-            index++;
+public static int search(int[] ar, int key){
+    int index = 0;
+    while(index < ar.length && key != ar[index]) {
+        index++;
+    }
+    return index == ar.length ? -1 : index;
+}
+public static int[] add(int [] ar, int number) {
+    int [] res = java.util.Arrays.copyOf(ar, ar.length + 1);
+    res[ar.length] = number;
+    return res;
+}
+/**
+ * 
+ * @param ar
+ * @param index
+ * @param number
+ * @return reference to a new array containing @param number at @param index
+ */
+public static int[] insert(int[] ar, int index, int number) {
+    //creates new array with all elements from the given "ar" and
+    //the given "number" at the given index
+    //to apply System.arraycopy method 
+    int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
+    System.arraycopy(ar, index, res, index + 1, ar.length - index);
+    res[index] = number;
+    return res; 
+}
+/**
+ * 
+ * @param numbers
+ * @param index
+ * @return new array with no removed from @param numbers number at @param index
+ */
+public static int[] remove(int[] numbers, int index) {
+    //creates new array with no element in "numbers" at "index"
+    //to apply System.arraycopy method 
+    int[] res = java.util.Arrays.copyOf(numbers, numbers.length - 1);
+    System.arraycopy(numbers, index + 1, res, index, res.length - index);
+    return res;
+
+}
+private static boolean pushMaxAtEnd(int [] ar, int length) {
+    boolean res = true;
+    for(int i = 0; i < length; i++) {
+        if (ar[i] > ar[i + 1]) {
+            res = false;
+            swap(ar, i, i + 1);
         }
-        return index == ar.length ? -1 : index;
-        // If index == ar.lenght, return -1, else return index
     }
-
-    public static int[] add(int[] ar, int number) {
-        int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
-        res[ar.length] = number;
-        return res;
+    return res;
+}
+private static void swap(int[] ar, int i, int j) {
+    int tmp = ar[i];
+    ar[i] = ar[j];
+    ar[j] = tmp;
+}
+public static void sort(int [] ar) {
+    int length = ar.length ;
+    boolean flSorted = false;
+    while(!flSorted) {
+        length--;
+        flSorted = pushMaxAtEnd(ar, length);
     }
+}
 
-    /*
-    * @param oldArray
-    * @param index
-    * @param number
-    * @return reference to a new array containing @param number at @param index
-    */
+/**
+ * 
+ * @param ar - sorted array
+ * @param key - being searched number 
+ * @return see comments definition
+ */
+public static int binarySearch(int [] ar, int key) {
+    //TODO
+    //index of the search key, if it is contained in the array;
+    // otherwise, (-(insertion point) - 1).
+    // The insertion point is defined as the point at which the key would be inserted into 
+    //the array: the index of the first element greater than the key, or a.length if all elements in the array are less than the specified key. Note that this guarantees that the return value will be >= 0 if and only if the key is found.
+    return -1;
+}
 
-    public static int[] insert(int[] oldArray, int index, int number) {
-        int[] newArray = new int[oldArray.length + 1];
-        if (index >= 0 && index <= oldArray.length) {
-            newArray[index] = number;
-            System.arraycopy(oldArray, 0, newArray, 0, index);
-            System.arraycopy(oldArray, index, newArray, index + 1, oldArray.length - index);    
-        }
-        return newArray;
-    }
+public static int[] insertSorted(int[] arSorted, int number) {
+    //TODO
+    //arSorted is sorted array
+    //to insert number at index to keep the array sorted
+    //additional sorting is disallowed
+    return null;
+}
+public static boolean isOneSwap(int [] array) {
+    //TODO
+    //return true if a given array has exactly one swap to get sorted array
+    //the swaped array's elements may or may not be neighbors 
+    return false;
 
-    /* 
-    * @param numbers
-    * @param index
-    * @return new array with no removed from @param numbers number at @param index
-    */
-
-    public static int[] remove(int[] numbers, int index) {
-        int[] newArray = new int[numbers.length];
-        if (index >= 0 && index <= newArray.length) {
-            System.arraycopy(numbers, 0, newArray, 0, index);
-            System.arraycopy(numbers, index + 1, newArray, index + 1, numbers.length - index - 1);    
-        }
-        return newArray;    
-    }
+}
 }
--- src/test/java/telran/util/test/ArraysTest.java
+++ src/test/java/telran/util/test/ArraysTest.java
@@ -1,50 +1,75 @@
 package telran.util.test;
 
-import static telran.util.Arrays.*;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static telran.util.Arrays.*;
+
+import java.util.Random;
 public class ArraysTest {
-    int[] numbers = {10, 7, 12, -4, 13, 3, 14};
+private static final int N_ELEMENTS = 1_000;
+int[] numbers = {10, 7, 12, -4, 13, 3, 14};
+@Test
+void searchTest() {
+    assertEquals(0, search(numbers, 10));
+    assertEquals(6, search(numbers, 14));
+    assertEquals(3, search(numbers, -4));
+    assertEquals(-1, search(numbers,100));
+}
+@Test
+void addTest() {
+    int newNumber = 100;
+    int [] expected = {10, 7, 12, -4, 13, 3, 14, newNumber};
+    assertArrayEquals(expected, add(numbers, newNumber));
+}
+@Test
+void insertTest(){
+    //{10, 7, 12, -4, 13, 3, 14} - all numbers
+    int newNumber = 30;
+    int[] expected_0 ={newNumber, 10, 7, 12, -4, 13, 3, 14};
+    int[] expected_3 = {10, 7, 12, newNumber, -4, 13, 3, 14};
+    int[] expected_last = {10, 7, 12,  -4, 13, 3, 14, newNumber};
+    assertArrayEquals(expected_0, insert(numbers, 0, newNumber));
+    assertArrayEquals(expected_3, insert(numbers, 3, newNumber));
+    assertArrayEquals(expected_last, insert(numbers, numbers.length, newNumber));
+    assertThrowsExactly(ArrayIndexOutOfBoundsException.class, ()->insert(numbers, numbers.length + 1, newNumber));
+    assertThrowsExactly(ArrayIndexOutOfBoundsException.class, ()->insert(numbers, -1, newNumber));
+}
+@Test
+void removeTest(){
+    //{10, 7, 12, -4, 13, 3, 14} - all numbers
+    int[] expected_0 ={ 7, 12, -4, 13, 3, 14};
+    int[] expected_3 = {10, 7, 12, 13, 3, 14};
+    int[] expected_last = {10, 7, 12, -4, 13, 3};
+    assertArrayEquals(expected_0, remove(numbers,0));
+    assertArrayEquals(expected_3, remove(numbers, 3));
+    assertArrayEquals(expected_last, remove(numbers, numbers.length-1));
+    assertThrowsExactly(ArrayIndexOutOfBoundsException.class, ()->remove(numbers, numbers.length));
+    assertThrowsExactly(ArrayIndexOutOfBoundsException.class, ()->remove(numbers, -1));
+}
 
-    @Test 
-    void searchTest() {
-        assertEquals(0, search(numbers, 10));
-        assertEquals(3, search(numbers, -4));
-        assertEquals(6, search(numbers, 14));
-        assertEquals(-1, search(numbers, 100));
+@Test
+void sortTest() {
+    int [] testNumbers = java.util.Arrays.copyOf(numbers, numbers.length);
+    int[] expected = {-4, 3, 7, 10,  12,  13,  14};
+    sort(testNumbers);
+    assertArrayEquals(expected, testNumbers);
+}
+@Test
+void sortTestRandomArray() {
+    int[] array = getRandomArray(N_ELEMENTS);
+    int limit = array.length - 1;
+    sort(array);
+    for(int i = 0; i < limit; i++) {
+        assertTrue(array[i] <= array[i + 1]);
     }
-
-    @Test 
-    void addTest() {
-        int newNumber = 100;
-        int[] expected = {10, 7, 12, -4, 13, 3, 14, 100};
-        assertArrayEquals(expected, add(numbers, newNumber));
+}
+private int[] getRandomArray(int nElements) {
+    int[] res = new int[nElements];
+    Random random = new Random();
+    for(int i = 0; i < nElements; i++) {
+        res[i] = random.nextInt();
     }
-
-    @Test 
-    void insertTest() {
-        int[] expected = {10, 7, 12, -4, 10000, 13, 3, 14};
-        assertArrayEquals(expected, insert(numbers, 4, 10000));
-        int[] expected2 = {10000, 10, 7, 12, -4, 13, 3, 14};
-        assertArrayEquals(expected2, insert(numbers, 0, 10000));
-        int[] expected3 = {10, 7, 12, -4, 13, 3, 14, 10000};
-        assertArrayEquals(expected3, insert(numbers, 7, 10000));
-        int[] expected4 = {0, 0, 0, 0, 0, 0, 0, 0};
-        assertArrayEquals(expected4, insert(numbers, 15, 10000));
-        assertArrayEquals(expected4, insert(numbers, -5, 10000));
-    }
-
-    @Test 
-    void removeTest() {
-        int[] expected = {10, 7, 12, -4, 0, 3, 14};
-        assertArrayEquals(expected, remove(numbers, 4));
-        int[] expected2 = {0, 7, 12, -4, 13, 3, 14};
-        assertArrayEquals(expected2, remove(numbers, 0));
-        int[] expected3 = {10, 7, 12, -4, 13, 3, 0};
-        assertArrayEquals(expected3, remove(numbers, 6));
-        int[] expected4 = {0, 0, 0, 0, 0, 0, 0};
-        assertArrayEquals(expected4, remove(numbers, 15));
-        assertArrayEquals(expected4, remove(numbers, -5));
-    }
+    return res;
+}
 }
